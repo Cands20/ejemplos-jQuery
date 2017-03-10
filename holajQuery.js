fd653cd64500a21d32886e52a9a3066bd0fa677c
Cands20 <sanchez.cesar95@gmail.com>
2017-03-10 12:57:14 -0600

    se agrego jQuery

---
+++ holajQuery.js
@@ -0,0 +1,20 @@
+/*$(document).ready(saluda);
+
+function saluda(){
+    alert($('h1').text());
+}*/
+
+
+/*$(function()
+{
+    //alert($('h1').text());
+    
+    $nuevo = $('<p>Parrafo <b>nuevo</b></p>');
+    $('h2').after($nuevo);
+
+    $('#btnUno').click(function (e) { 
+        e.preventDefault();
+        
+    });
+})*/
+
